134d2380683fa78ae37603a4b6d416e2094b980f
Stettzy <{ID}+{username}@users.noreply.github.com>
2023-10-20 15:05:25 +0200

    composition api & local storage composite

---
+++ 6. composition_api/1.mixins_to_composables/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { useFlash } from "@/composables/useFlash";
+
+let { flash } = useFlash();
+</script>
+
+<template>
+  <main>
+    <p><button @click="flash('oki')">test</button></p>
+  </main>
+</template>
---
+++ 6. composition_api/2.composable_example_local_storage/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref, watch } from "vue";
+import { useLocalStorage } from "@/composables/useLocalStorage.js";
+
+let food = useLocalStorage("food"),
+  age = useLocalStorage("age");
+</script>
+
+<template>
+  <main>
+    <p>
+      What's your favorite food?
+      <input type="text" v-model="food" />
+    </p>
+    <p>What's your age? <input type="text" v-model="age" /></p>
+  </main>
+</template>
